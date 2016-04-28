abe661b88210dc92ff8e7cd4c5e401a0f178a1c6
erebuseternal <marcelsanders96@gmail.com>
2016-04-28 16:42:57 -0400

    continued work, things seem to work with a few hiccups I am fixing. But the fixes for the hiccups are deep enough that I am just going to go through and clean up the code next

--- kirky/kirky/test.py
+++ kirky/kirky/test.py
@@ -2,11 +2,26 @@ from linear import Slice, Lattice
 from pyx import canvas, path
 from math import pi, sqrt
 
-slice = Slice(pi/6, pi/7)
-print('slice stuff %s %s' % (slice.upper[0], slice.lower[0]))
+slice = Slice(pi/4, pi/4)
 lattice = Lattice()
+lattice.FindPoint(slice,1)
+c = canvas.canvas()
+lattice.Draw(c)
+max_sum = 0
+for point in lattice.points:
+    sum = point.position[0] ** 2 + point.position[1] ** 2
+    if sum > max_sum:
+        max_sum = sum
+slice.Draw(c, sqrt(max_sum))
+"""
 lattice.GrowAbout([0,0])
+lattice.GrowNext(slice)
 c = canvas.canvas()
 lattice.Draw(c)
 slice.Draw(c, sqrt(2))
+lattice.FindMarkedPoints(slice)
+for point in lattice.marked:
+    print(point.position)
+print(slice.inclusion_bounds)
+"""
 c.writePDFfile('drawing')
